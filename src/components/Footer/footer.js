ee64caf0df4ab6e4ede9d322e3cf973bde2bdf8b
Blueberry Pearl <120589100+BlueberryPearl@users.noreply.github.com>
2023-04-24 15:00:47 +0100

    Updated the size in css and changed twitter icon.

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ReactComponent as InstagramLogo } from './Instagram Icon.svg';
-import { ReactComponent as TwitterIcon } from './Twitter Icon.svg';
+import { ReactComponent as YoutubeLogo } from './youtube.svg';
 import { ReactComponent as GitHubIcon } from './GitHub Icon.svg';
 
 export default function Footer() {
     return (
         <div className='footer'>
-            <a href="https://twitter.com/Najettechou" target="_blank" rel="noopener noreferrer"><TwitterIcon className='twitter-icon' /></a>
+            <a href="https://www.youtube.com/@blueberrypearl7087" target="_blank" rel="noopener noreferrer"><YoutubeLogo className='youtube-icon' /></a>
             <a href="https://www.instagram.com/blue.berrypearl/" target="_blank" rel="noopener noreferrer"><InstagramLogo className='instagram-icon' /></a>
             <a href="https://github.com/BlueberryPearl" target="_blank" rel="noopener noreferrer"><GitHubIcon className='gitHub-icon' /></a>
         </div>
