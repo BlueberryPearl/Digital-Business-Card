6e6dda0e364f7970149e1d5fa305b75fe330982c
Blueberry Pearl <120589100+BlueberryPearl@users.noreply.github.com>
2023-04-28 13:06:47 +0100

    Changed the website to Portfolio and added icon.

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -8,7 +8,7 @@ export default function Info() {
             <img src={Photo} className='profilePic' alt='Profile' />
             <h2>Najette Chouchane</h2>
             <p>Frontend Developer</p>
-            <a href='https://www.najette.co.uk/' target="_blank" rel="noopener noreferrer" className='website'>najette.co.uk</a>
+            <a href='https://blueberrypearl.github.io/Portfolio/' target="_blank" rel="noopener noreferrer" className='website'><i class="fa-solid fa-laptop-code fa-sm"></i> Portfolio  </a>
             <div className='buttons'>
                 <a href='mailto: najette@chouchane.com' className='email'><i class="fa-solid fa-envelope"></i>  Email</a>
                 <a href='https://www.linkedin.com/in/najette-chouchane-a939abb9/' target="_blank" rel="noopener noreferrer" className='linkedIn'><i class="fa-brands fa-linkedin"></i>  LinkedIn</a>
